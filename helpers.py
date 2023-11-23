0738977bbc2ca25353960e182dd2bd4b96334f74
viols-code <viola.renne@gmail.com>
2023-11-23 19:40:56 +0100

    feature: modify helpers.py

--- helpers.py
+++ helpers.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import numpy as np
-from run import *
+
+# Paths
+lipid_path = 'data/lba_all_pixels_fully_abamapped11062023.h5'
+gene_path = 'data/cell_filtered_w500genes.h5'
 
 
 def loading_lipids(linear_scale=True):
+    """
+    Load lipids dataset
+
+    Parameters:
+    :param linear_scale: True for a linear scale, False for an exponential scale
+
+    Returns:
+    :return: lipids dataset
+    """
     # Loading the dataset
     lipids = pd.read_hdf(lipid_path)
 
@@ -17,10 +29,44 @@ def loading_lipids(linear_scale=True):
     return lipids
 
 
-def select_section(lipids, number=12):
-    """ Select data in the given section of the brain """
+def loading_genes():
+    """
+    Load genes dataset
+
+    Parameters:
+    :return: gene dataset
+    """
+    cells = pd.read_hdf(gene_path, 'df')
+
+    return cells
+
+
+def select_section_lipids(lipids, number=12):
+    """
+    Select lipids data in the given section of the brain
+
+    Parameters:
+    :param lipids: lipids dataset
+    :param number: section number
+
+    Returns:
+    :return: lipids dataset with only the given section
+    """
     section = lipids.loc[lipids['Section'] == number]
     return section
 
-def compute_euclidian_distance(x_lipid, y_lipid, x_gene, y_gene):
-    pass
+
+def select_section_genes(cells):
+    """
+    Select genes data in the given section of the brain
+
+    Parameters:
+    :param cells: genes dataset
+
+    Returns:
+    :return: genes dataset with only the 12 section
+    """
+    cells_section_12 = cells.loc[(cells['x_ccf'] > 7.4) & (cells['x_ccf'] < 7.8), :]
+
+    return cells_section_12
+
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
+from helpers import *
 
+if __name__ == '__main__':
+    lipid = loading_lipids()
+    gene = loading_genes()
 
-# Paths
-lipid_path = 'data/lba_all_pixels_fully_abamapped11062023.h5'
+    section_12_lipids = select_section_lipids(lipid)
+    section_12_genes = select_section_genes(gene)

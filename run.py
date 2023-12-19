6dc70d2e110c0a206fc95912378849abb4acb5af
viols-code <viola.renne@gmail.com>
2023-12-19 21:28:31 +0100

    feature: final neural network version

--- run.py
+++
@@ -1,8 +0,0 @@
-from helpers import *
-
-if __name__ == '__main__':
-    lipid = load_lipids()
-    gene = load_genes()
-
-    section_12_lipids = select_section_lipids(lipid)
-    section_12_genes = select_section_genes(gene)
